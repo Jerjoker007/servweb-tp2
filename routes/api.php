858bf2b59ad06f16f041d8da404b24f139279151
Jeremie-Paquin <paquin.jeremi@gmail.com>
2026-03-30 19:05:45 -0400

    Ajouté les routes avec les middleware

--- routes/api.php
+++ routes/api.php
@@ -8,4 +8,12 @@ use App\Http\Controllers\AuthController;
 
 
 
-
+Route::middleware('throttle:5,1')->group(function () {
+    Route::post('/signup', 'App\Http\Controllers\AuthController@register');
+    Route::post('/signin', 'App\Http\Controllers\AuthController@login');
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::get('/me', 'App\Http\Controllers\AuthController@getUser');
+        Route::post('/refresh', 'App\Http\Controllers\AuthController@refreshToken');
+        Route::post('/signout', 'App\Http\Controllers\AuthController@logout');
+    });
+});
