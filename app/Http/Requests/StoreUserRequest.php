e32405c146c196942e6b39bc596b3aae1a55a7a9
Jeremie-Paquin <paquin.jeremi@gmail.com>
2026-03-30 14:56:53 -0400

    Ajout des validations des requêtes de signin et login

---
+++ app/Http/Requests/StoreUserRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Password;
+
+class StoreUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required | string',
+            'email' => 'required | email:rfc,strict,filter | unique:users',
+            'login' => 'required | unique:users',
+            //https://laravel.com/docs/12.x/validation#validating-passwords
+            'password' => 'required | Password::min(10)->letters()->numbers()'
+        ];
+    }
+}
